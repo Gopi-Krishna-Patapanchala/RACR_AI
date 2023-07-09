d4ed794285bba2b78d6e9911cf670fa36a294a43
S Piccolo <stevepic95@gmail.com>
2023-07-09 00:21:12 -0400

    basic experiment framework

--- api/device.py
+++ api/device.py
@@ -486,30 +486,6 @@ class DeviceManager:
             )
 
 
-class Experiment:
-    def __init__(self, name, code_dir, config):
-        self.name = name
-        self.code_dir = code_dir
-        self.config = config
-
-    def run(self, parameters):
-        pass  # Implement experiment running
-
-
-class ExperimentManager:
-    def __init__(self):
-        self.experiments = []
-
-    def create_experiment(self, name, code_dir, config):
-        experiment = Experiment(name, code_dir, config)
-        self.experiments.append(experiment)
-
-    def run_experiment(self, name, parameters):
-        for experiment in self.experiments:
-            if experiment.name == name:
-                experiment.run(parameters)
-
-
 if __name__ == "__main__":
     dm = DeviceManager()
     for device in dm.devices:
